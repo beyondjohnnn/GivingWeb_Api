992821e9f3dd9f1d9e4d1f53534b11b84c89c502
lost-in-Code-au <lost-in-Code-au>
2017-10-19 17:14:51 +0100

    updated charity feat members controller to reduce rails responce time

--- app/controllers/charity_featured_members_controller.rb
+++ app/controllers/charity_featured_members_controller.rb
@@ -1,42 +1,30 @@
 class CharityFeaturedMembersController < ApplicationController
 
-    def show
-      featured_members = CharityFeaturedMember.where(params[charity_id: params[:id]]).order(:position)
-
-      featured_members = featured_members.map do |members|
-        members.member.as_json({
-          include: [:donations, :comments]
-        })
-      end
-      render json: featured_members
+  def renderMember(charity_id)
+    featured_members = CharityFeaturedMember.where(charity_id: charity_id).order(:position)
+    featured_members = featured_members.map do |members|
+      members.member.as_json({
+        include: [:donations, :comments]
+      })
     end
+    return featured_members
+  end
 
-    def create
-      featured_member = CharityFeaturedMember.new({ charity_id: params[:charity_id], member_id: params[:member_id], position: params[:position]})
-      if featured_member.save
-        render json: featured_member
-      else
-        render json: {errors: "did not save"}
-      end
+  def show
+    render json: renderMember(params[:id])
+  end
+
+  def create
+    featured_member = CharityFeaturedMember.new({ charity_id: params[:charity_id], member_id: params[:member_id], position: params[:position]})
+    if featured_member.save
+      render json: renderMember(params[:charity_id])
+    else
+      render json: {errors: "did not save"}
     end
+  end
 
-    def update
-      member = CharityFeaturedMember.where(member_id: params[:id])[0]
-      member.position = params[:position]
-      member.save
-      render json: member
-    end
-
-    def destroy
-      find_charity_id = CharityFeaturedMember.where(member_id: params[:id])[0].delete
-      # delete = CharityFeaturedMember.where(member_id: params[:id])[0].delete
-      featured_members = CharityFeaturedMember.where(charity_id: find_charity_id.charity_id).order(:position)
-
-      featured_members = featured_members.map do |members|
-        members.member.as_json({
-          include: [:donations, :comments]
-        })
-      end
-      render json: featured_members
-    end
- end
+  def destroy
+    find_charity_id = CharityFeaturedMember.where(member_id: params[:id])[0].delete
+    render json: renderMember(find_charity_id.charity_id)
+  end
+end
