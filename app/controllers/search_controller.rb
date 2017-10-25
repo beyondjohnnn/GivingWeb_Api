7b2f91452d190cc15a70160ce60a6a34cab9d277
eoghanscrowley <eoghanscrowley@gmail.com>
2017-10-25 10:20:05 +0100

    clean up search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,24 @@
 class SearchController < ApplicationController
+
   def show
+
     key = "%#{params[:searchTerm].downcase}%"
-    @member_results =
-    Member.where(
+
+    @member_results = Member.limit(3).where(
       'lower(name) LIKE ? OR lower(info) LIKE ? OR lower(snippet) LIKE ? OR lower(location) LIKE ?',
-      key, key, key, key).order(:name)
-    @charity_results = Charity.where(
+      key,
+      key,
+      key,
+      key
+    ).order(:name)
+
+    @charity_results = Charity.limit(3).where(
       'lower(name) LIKE ? OR lower(description) LIKE ?',
-      key, key).order(:name)
+      key,
+      key
+    ).order(:name)
 
     render json: ({ members: @member_results, charities: @charity_results })
   end
+
 end
