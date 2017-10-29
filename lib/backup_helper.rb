34c7e5b93781b22dc372c635e1d3c455d07a1e0f
James Cox <j-cox03@hotmail.com>
2017-10-29 14:51:32 +0000

    Added methods on backup helper for performing acutal backup

--- lib/backup_helper.rb
+++ lib/backup_helper.rb
@@ -4,11 +4,16 @@ class BackUpHelper
 
   attr_reader :directory, :backup_time, :folder
 
-  def initialize(target_directory)
+  def initialize(database, target_directory)
+    @database = database
     @directory = target_directory
     @backup_time = get_time()
     @folder = "#{@directory}backup-#{@backup_time}/"
-    build_backup_directory()
+  end
+
+  def use_current_time()
+    @backup_time = get_time()
+    @folder = "#{@directory}backup-#{@backup_time}/"
   end
 
   def get_backup_file_name()
@@ -17,6 +22,18 @@ class BackUpHelper
 
   def get_log_file_name()
     return get_file_name_using_id("log")
+  end
+
+  def make_backup()
+    build_backup_directory()
+    system("pg_dump #{@database} > #{get_backup_file_name()}.sql")
+    log_database("#{get_log_file_name()}.txt")
+  end
+
+  def make_zipped_backup()
+    build_backup_directory()
+    system("pg_dump #{@database} | gzip > #{get_backup_file_name()}.gz")
+    log_database("#{get_log_file_name()}.txt")
   end
 
   private
